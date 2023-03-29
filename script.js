62ebf9eab86bb418de18db3d24a988931f4ca0c9
joao pedro soares figueiredo <100363401+Jpsoaresfig@users.noreply.github.com>
2023-03-29 09:45:19 -0300

    Update script.js
    colocando algumas anotaçoes para decorar

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 async function minhaFuncao() {
     const local = document.getElementById('meuContent')
 
-    const response = await fetch('https://api.randomuser.me/?results=10')
-    const data = await response.json()
+    const response = await fetch('https://api.randomuser.me/?results=10')//receber a api
+    const data = await response.json()//converter para json
 
     const meuArray = data.results
 
-    for(i = 0; i < meuArray.length; i++) {
-        const email = meuArray[i].email
-        const name = meuArray[i].name.first
-        const secondName = meuArray[i].name.last
+    for(i = 0; i < meuArray.length; i++) {//for para leitura de cada intem que ele pede no array
+        const email = meuArray[i].email//item email
+        const name = meuArray[i].name.first//item primeiro nome
+        const secondName = meuArray[i].name.last//item ultimo nome
 
-        const divMeuCard = document.createElement('div')
-        local.appendChild(divMeuCard)
+        const divMeuCard = document.createElement('div')//criaçao de div 
+        local.appendChild(divMeuCard)//quero colocar divmeucard em local
     
-        const meuH1 = document.createElement('h1')
-        meuH1.innerText = `${name} ${secondName}`
-        meuH1.style.color = 'red'
-        meuH1.style.fontSize = '1rem'
-        divMeuCard.appendChild(meuH1)
+        const meuH1 = document.createElement('h1')//criando h1
+        meuH1.innerText = `${name} ${secondName}`//inserir váriaveis do array 
+        meuH1.style.color = 'red'//css
+        meuH1.style.fontSize = '1rem'//css
+        divMeuCard.appendChild(meuH1)//quero colocar h1 no divMeuCard
     
-        const meuParagrafo = document.createElement('p')
-        meuParagrafo.innerText = email
-        divMeuCard.appendChild(meuParagrafo)
+        const meuParagrafo = document.createElement('p')//criando paragrafo
+        meuParagrafo.innerText = email//no maragrafo bote o email da array
+        divMeuCard.appendChild(meuParagrafo)//dentro do Meu paragrafo quero colocar o divMeucard
     }
     
     
